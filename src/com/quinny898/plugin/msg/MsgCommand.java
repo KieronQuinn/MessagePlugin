c3dc00bb6ef552dec2eec71b43def27dc0898796
Rens Rikkerink <ikkerens@hotmail.com>
2014-03-04 20:02:56 +0100

    Fixed an issue where messages were only allowed to be 1 in length

--- src/com/quinny898/plugin/msg/MsgCommand.java
+++ src/com/quinny898/plugin/msg/MsgCommand.java
@@ -1,5 +1,7 @@
 package com.quinny898.plugin.msg;
 
+import java.util.Arrays;
+
 import com.mbserver.api.CommandExecutor;
 import com.mbserver.api.CommandSender;
 import com.mbserver.api.Server;
@@ -12,15 +14,19 @@ public class MsgCommand implements CommandExecutor {
 			String label) {
 		Server server = Msg.context.getServer();
 		Player player = server.getPlayer(args[0]);
-		if (args.length != 2) {
+		if (args.length < 2) {
 			sender.sendMessage("Usage: /msg <name> <message>");
 		} else {
+			String message = " ";
+			for( String arg : Arrays.copyOfRange(args, 2, args.length) )
+				message += arg;
+
 			if (player != null) {
 				player.sendMessage("Msg from " + sender.getName() + ": "
-						+ args[1] + "\nUse /msg " + sender.getName()
+						+ message + "\nUse /msg " + sender.getName()
 						+ " to reply");
 				sender.sendMessage(sender.getName() + " > " + player.getName()
-						+ " "+ args[1]);
+						+ " "+ message);
 			} else {
 				sender.sendMessage("Error: Player not found");
 			}
