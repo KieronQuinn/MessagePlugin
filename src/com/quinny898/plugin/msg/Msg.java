10aeac136b019edbc606b1e799dd2025b0f0a2d1
Kieron Quinn <quinny898@gmail.com>
2014-03-04 19:43:01 +0000

    Changed manifest

--- src/com/quinny898/plugin/msg/Msg.java
+++ src/com/quinny898/plugin/msg/Msg.java
@@ -3,7 +3,7 @@ package com.quinny898.plugin.msg;
 import com.mbserver.api.MBServerPlugin;
 import com.mbserver.api.Manifest;
 
-@Manifest(name = "Private Message by Quinny898")
+@Manifest(name = "PrivateMessaging", authors = "Quinny898")
 public class Msg extends MBServerPlugin {
 	static Msg context;
 
